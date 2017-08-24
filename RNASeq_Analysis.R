dfc8348fb5bc585ef5cc8774f502c0be5b742308
Sruthi Mouli <31308204+SruthiMouli@users.noreply.github.com>
2017-08-24 15:53:02 +0300

    Add files via upload

--- RNASeq_Analysis.R
+++ RNASeq_Analysis.R
@@ -16,12 +16,13 @@ data_mat = data_xls[,c(1,2:4,6:8)]
 write.csv(data_mat,sep='\t',file = "Data_for_AnalyzeDE.csv")
 #Format in DESeq readable matrix
 ##Select required data and read to matrix
-DESeq_res=analyzeDE(fileName = 'Data_for_AnalyzeDE.csv',c("D25minus","D25minus","D25minus","D25plus","D25plus","D25plus"),"D25minus","D25plus",0.05,1)
-
-## Replace row.names from ensembl Ids to external names
-result_Data = merge(DESeq_res,data_xls,by = "names.dds.")
-result_Data = result_Data[order(result_Data$log2FoldChange,decreasing = TRUE),]
-vsd_exprs= cbind(result_Data$vsd_ctrl,result_Data$vsd_case)
-row.names(vsd_exprs) = result_Data$external_gene_name
-colnames(vsd_exprs) = c("vsd_ctrl","vsd_case")
-heatmap.2(vsd_exprs, trace="none", margin=c(10, 6),scale='row')
+DESeq_res = analyzeDE(fileName = 'Data_for_AnalyzeDE.csv',c("D25minus","D25minus","D25minus","D25plus","D25plus","D25plus"),"D25minus","D25plus",0.05,1)
+##Get the last 6 columns
+res = DESeq_res[,(ncol(DESeq_res)-6+1):ncol(DESeq_res)]
+hmcol = colorRampPalette(brewer.pal(11, "RdYlGn"))(100)
+res=as.matrix(res)
+## Heat map for all six values
+heatmap.2(res,Colv=F,col=hmcol,scale='row',dendrogram='row',key=T,density.info='none',trace='none')
+## Take row means of two conditions
+Exp_Mean= cbind(rowMeans(x[,c(1:3)]),rowMeans(x[,c(4:6)]))
+heatmap.2(Exp_Mean,Colv=F,col=hmcol,scale='row',dendrogram='row',key=T,density.info='none',trace='none')
--- analyzeDE.R
+++ analyzeDE.R
@@ -14,32 +14,24 @@
 #' @importFrom utils read.csv write.csv
 #' @examples
 #' analyzeDE('Example_SeqFile.txt',c('Case1','Case1','Case1','Control','Control','Control'),'Control','Case1',0.05,1)
-biocLite('DESeq')
-library('DESeq')
 analyzeDE <- function(fileName,condVec,controlName,caseName,adjPVal,logFC) {
-  a <- utils::read.csv(fileName,header = T)
-  b <- a[,2:ncol(a)]
-  if(is.vector(condVec) == FALSE){print('Conditions should be in a vector format. Pls use c()')}
-  if(length(condVec) != ncol(b)){print('Conditions vector should have the same length as the conditions in the Seq data')}
-  row.names(b) <- a[,1]
-  countTable <- as.matrix(b[rowSums(b) != 0, ])
-  storage.mode(countTable) = 'integer'
-  cds <- DESeq::newCountDataSet(countTable, factor(condVec))
-  cds <- DESeq::estimateSizeFactors(cds)
-  cds <- DESeq::estimateDispersions(cds, method = 'pooled', sharingMode = 'maximum', fitType = 'local')
-  vsd <- DESeq::getVarianceStabilizedData(cds)
-  mod_lfc <- (rowMeans(vsd[,conditions(cds) == caseName, drop = FALSE]) - rowMeans(vsd[,conditions(cds) == controlName, drop = FALSE]))
-  res <- DESeq::nbinomTest(cds, controlName, caseName)
-  res <- cbind(res, mod_lfc)
-  res2 <- res[which(res$padj < adjPVal),]
-  res3 <- res2[which(abs(res2$mod_lfc) > logFC),]
-  vsd_sel=vsd[row.names(res3),]
-  vsd_case = vsd_sel[,conditions(cds) == caseName, drop = FALSE]
-  vsd_case_Mean=rowMeans(vsd_case) 
-  names(vsd)
-  vsd_ctrl = vsd_sel[,conditions(cds) == controlName, drop = FALSE]
-  vsd_ctrl_Mean=rowMeans(vsd_ctrl)
-  vsdFull = varianceStabilizingTransformation( cds )
-  res_final=cbind(vsd_ctrl_Mean,vsd_case_Mean,res3)
-  finalList = list(vsd = vsdFull, cds = cds, res_final = res_final)
-  return(finalList)}
+    a <- utils::read.csv(fileName,header = T)
+    b <- a[,2:ncol(a)]
+    if(is.vector(condVec) == FALSE){print('Conditions should be in a vector format. Pls use c()')}
+    if(length(condVec) != ncol(b)){print('Conditions vector should have the same length as the conditions in the Seq data')}
+    row.names(b) <- a[,1]
+    countTable <- as.matrix(b[rowSums(b) != 0, ])
+    storage.mode(countTable) = 'integer'
+    cds <- DESeq::newCountDataSet(countTable, factor(condVec))
+    cds <- DESeq::estimateSizeFactors(cds)
+    cds <- DESeq::estimateDispersions(cds, method = 'pooled', sharingMode = 'maximum', fitType = 'local')
+    vsd <- DESeq::getVarianceStabilizedData(cds)
+    mod_lfc <- (rowMeans(vsd[,conditions(cds) == caseName, drop = FALSE]) - rowMeans(vsd[,conditions(cds) == controlName, drop = FALSE]))
+    res <- DESeq::nbinomTest(cds, controlName, caseName)
+    res <- cbind(res, mod_lfc)
+    res2 <- res[which(res$padj < adjPVal),]
+    res3 <- res2[which(abs(res2$mod_lfc) > logFC),]
+    vsd1 <- vsd[res3$id,conditions(cds) %in% c(controlName,caseName)]
+    res4 <- cbind(res3,vsd1)
+    write.csv(res4,paste0('DE_',caseName,'-',controlName,'.csv'),row.names = FALSE)
+    return(res4)}
